af8f3bb980b4d33b4c11820596433a4d176f5caf
Shalom Ogoziem <shalomzymike@gmail.com>
2023-08-11 11:28:01 +0000

    state

---
+++ models/state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Defines a Module State that inherits from Base"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    inherits from the BaseModel
+    """
+    name = ""
